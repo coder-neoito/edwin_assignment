6156501d128ca2930477d6e1f9568e4c9068eed5
edwin-neoito <edwin.c@neoito.com>
2023-11-09 07:47:46 +0530

    Revert "feat: basic course listing layout and single course view"
    This reverts commit 9fb138ce5bdd50a3101ee6643c782d60b6b8a9e7.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import NavBar from "components/NavBar";
-import Router from "navigation/router";
-
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Router/>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
     </div>
   );
 }
