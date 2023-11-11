e0d6e1e6bb72f5ac52a3f2881c5cf6fd31e5e0ec
edwin-neoito <edwin.c@neoito.com>
2023-11-12 00:55:23 +0530

    fix: replaced pogr text with logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 const Header = () => {
-  return <div className="w-full bg-[#292936] flex justify-end items-center gap-3 p-[18px]">
+  return <div className="w-full bg-[#292936] flex justify-end items-center gap-3 p-4">
     <button className="header-button text-white bg-[#4A4A5F]">Sign in</button>
     <button className="bg-white text-[#292936] header-button">Register</button>
   </div>;
--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -1,9 +1,11 @@
-import { Home, News } from 'assets/Images';
+import { Home, Logo, News } from 'assets/Images';
 
 const SideNav = () => {
   return (
     <div className="h-full bg-[#292936] flex flex-col w-[260px]">
-      <div className="w-full p-6 text-white font-bold text-3xl">POGR</div>
+      <div className="w-full p-6 text-white font-bold text-3xl">
+        <img src={Logo} alt="" />
+      </div>
       <ul className="flex flex-col list-none nav-list w-full">
         <li>
           <img src={Home} alt="" width={20} height={20} />
